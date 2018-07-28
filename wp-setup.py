99fbc249200ecab587b7c4abdcb84350f3fb2d80
nakano <nakano@esu2.co.jp>
2018-07-28 13:45:14 +0900

    modified config.

--- wp-setup.py
+++ wp-setup.py
@@ -1,19 +1,41 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
+import sys
+import re
+import urllib.request
 from ConfigParser import SafeConfigParser
 
 configfile = 'config.ini'
+tmpdir = '/tmp/'
 
 def main():
-    print getParam("user")
-    print getParam("password")
-    print getParam("host")
+    print getParam("mysql", "user")
+    print getParam("mysql", "password")
+    print getParam("mysql", "host")
+    print getParam("init", "path")
 
-def getParam(param_name):
+def getParam(section, param_name):
     parser = SafeConfigParser()
     parser.read(configfile)
-    return parser.get('mysql', param_name)
+    return parser.get(section, param_name)
+
+def downloadWP():
+    ver = getParam("init", "version")
+    # https://ja.wordpress.org/latest-ja.tar.gz
+    # https://ja.wordpress.org/wordpress-4.9.7-ja.tar.gz
+    url = "https://ja.wordpress.org/"
+
+    regex = r'^\d\.\d\+.\d+$'
+
+    if re.match(regex, ver):
+        url += "wordpress-" + ver + "-ja.tar.gz"
+    else :
+        url += "latest-ja.tar.gz"
+
+    print url
+
+    urllib.request.urlretrieve(url, tmpdir + "latest-ja.tar.gz")
 
 if __name__ == '__main__':
     main()
